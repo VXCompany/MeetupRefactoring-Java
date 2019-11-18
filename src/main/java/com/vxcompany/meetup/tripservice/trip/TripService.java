6a122a3efd6e56d80657b7f84afc220a88b339d1
Sander Verbruggen <sverbruggen@vxcompany.com>
2019-11-18 20:11:38 +0100

    loggedInUser als parameter

--- src/main/java/com/vxcompany/meetup/tripservice/trip/TripService.java
+++ src/main/java/com/vxcompany/meetup/tripservice/trip/TripService.java
@@ -2,30 +2,23 @@ package com.vxcompany.meetup.tripservice.trip;
 
 import com.vxcompany.meetup.tripservice.exception.UserNotLoggedInException;
 import com.vxcompany.meetup.tripservice.user.User;
-import com.vxcompany.meetup.tripservice.user.UserSession;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TripService {
 
-    public List<Trip> getTripsByUser(final User user) throws UserNotLoggedInException {
-        final User loggedUser = getLoggedUser();
-        if (loggedUser != null) {
-            return user.isFriendsWith(loggedUser)
-                    ? tripsBy(user)
-                    : new ArrayList<>();
-        } else {
+    public List<Trip> getTripsByUser(final User user, final User loggedInUser) throws UserNotLoggedInException {
+        if (loggedInUser == null) {
             throw new UserNotLoggedInException();
         }
+
+        return user.isFriendsWith(loggedInUser)
+                ? tripsBy(user)
+                : new ArrayList<>();
     }
 
     protected List<Trip> tripsBy(final User user) {
         return TripDAO.findTripsByUser(user);
     }
-
-    protected User getLoggedUser() {
-        return UserSession.getInstance().getLoggedUser();
-    }
-
 }
--- src/test/java/com/vxcompany/meetup/tripservice/trip/TripServiceTest.java
+++ src/test/java/com/vxcompany/meetup/tripservice/trip/TripServiceTest.java
@@ -3,6 +3,7 @@ package com.vxcompany.meetup.tripservice.trip;
 import com.vxcompany.meetup.tripservice.exception.UserNotLoggedInException;
 import com.vxcompany.meetup.tripservice.user.User;
 import com.vxcompany.meetup.tripservice.user.UserBuilder;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -17,50 +18,39 @@ class TripServiceTest {
     private static final User GUEST = null;
     private static final User REGISTERED_USER = new User();
     private static final Trip TO_BAARN = new Trip();
+    private TripService tripService;
+
+    @BeforeEach
+    void setUp() {
+        tripService = new TestableTripService();
+    }
 
     @Test
     void should_throw_exception_when_user_is_not_logged_in() {
-        final TripService tripService = new TestableTripService(GUEST);
-
         assertThrows(UserNotLoggedInException.class,
-                () -> tripService.getTripsByUser(A_USER));
+                () -> tripService.getTripsByUser(A_USER, GUEST));
     }
 
     @Test
     void should_return_no_trips_from_strangers() {
-        final TripService tripService = new TestableTripService(REGISTERED_USER);
-
-        final List<Trip> trips = tripService.getTripsByUser(A_USER);
+        final List<Trip> trips = tripService.getTripsByUser(A_USER, REGISTERED_USER);
 
         assertTrue(trips.isEmpty());
     }
 
     @Test
     void should_return_trips_from_friends() {
-        final TripService tripService = new TestableTripService(REGISTERED_USER);
         final User befriendedUser = UserBuilder.aUser()
                 .withFriends(REGISTERED_USER, A_USER)
                 .withTrips(TO_BAARN)
                 .build();
 
-        final List<Trip> trips = tripService.getTripsByUser(befriendedUser);
+        final List<Trip> trips = tripService.getTripsByUser(befriendedUser, REGISTERED_USER);
 
         assertEquals(1, trips.size());
     }
 
     private static class TestableTripService extends TripService {
-
-        private final User loggedInUser;
-
-        private TestableTripService(final User loggedInUser) {
-            this.loggedInUser = loggedInUser;
-        }
-
-        @Override
-        protected User getLoggedUser() {
-            return loggedInUser;
-        }
-
         @Override
         protected List<Trip> tripsBy(final User user) {
             return user.trips();
